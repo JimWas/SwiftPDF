702b0f4486c281ea6edfe986594633572096d5ee
Jim Washkau <jim@jimwashkau.com>
2026-02-22 11:35:28 +0900

    Initial Commit

---
+++ SwiftPDF/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftPDF
+//
+//  Created by Jim Washkau on 2/22/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
